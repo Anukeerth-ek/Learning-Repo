56bd90bbaee19d549b40c347847352648aef8160
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-10-08 21:50:45 +0530

    Added graphql introduction part:

---
+++ src/GRAPH QL/index.js
@@ -0,0 +1,5 @@
+// _______________________________________________________________
+// 1. What is graphql?
+// ans: Graphql is a query language and a server side runtime for executing query using a type system you define for your data. GraphQL isn’t tied to any 
+// specific database or storage engine and is instead backed by your existing code and data.
+// _______________________________________________________________
