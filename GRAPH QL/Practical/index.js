901f188416093b675769c3e8e2bee8661d15faca
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-11-24 09:53:47 +0530

    Feat(new learning): Learned how to pass variable

--- GRAPH QL/Practical/index.js
+++ GRAPH QL/Practical/index.js
@@ -1,37 +1,39 @@
 import { ApolloServer } from "@apollo/server";
-import {startStandaloneServer} from '@apollo/server/standalone'
-import { gql } from 'graphql-tag';
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { gql } from "graphql-tag";
 import _db from "./_db.js";
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
 // types
 const typeDefs = gql`
-  ${readFileSync(new URL('./schema.gql', import.meta.url), 'utf-8')}
+     ${readFileSync(new URL("./schema.gql", import.meta.url), "utf-8")}
 `;
 
 const resolvers = {
-    Query: {
-       reviews(){
-         return  _db.reviews
-       },
-       games(){
-        return _db.games
-       },
-       authors(){
-        return _db.authors
-       }
-    }
-}
+     Query: {
+          reviews() {
+               return _db.reviews;
+          },
+          games() {
+               return _db.games;
+          },
+          authors() {
+               return _db.authors;
+          },
+          review(_, args) {
+               return _db.reviews.find((review)=> review.id === args.id)
+          },
+     },
+};
 
 // server setup
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
+     typeDefs,
+     resolvers,
+});
 
-
-const {url} = await startStandaloneServer(server, {
-    listen:{port: 4000}
-})
+const { url } = await startStandaloneServer(server, {
+     listen: { port: 4000 },
+});
 
 console.log(`Server running at ${url}`);
