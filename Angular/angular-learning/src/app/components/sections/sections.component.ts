7f815bee4c06d757783c89c969009dab0548bda0
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-25 20:23:34 +0530

    Learn: how to pass data to child component

--- Angular/angular-learning/src/app/components/sections/sections.component.ts
+++ Angular/angular-learning/src/app/components/sections/sections.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CounterComponent } from '../counter/counter.component';
 
 @Component({
   selector: 'app-sections',
-  imports: [],
+  imports: [CounterComponent],
   templateUrl: './sections.component.html',
   styleUrl: './sections.component.css',
   
 })
 export class SectionsComponent {
-
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`User has typed something ${event.isTrusted}`)
+  }
 }
